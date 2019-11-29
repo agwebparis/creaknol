19dd596bd626c8e9d5ba744a48dc26f919bb2fc0
aknol <dev.creaknol@gmail.com>
2019-11-29 14:12:05 +0100

    Commentaire supprimer

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
-// import "./styles.scss";
-//console.log("hello world!");
-//window.onload = function(){ {}
-
-
-
-
 import loadNavMenu from './js/nav-menu';
 
 window.addEventListener('DOMContentLoaded', (event) => {
   loadNavMenu();
 });
-
-
-
-
-// function openNav() {
-//     document.getElementById("myNav").style.width = "100%";
-// }
-
-// function closeNav() {
-//     document.getElementById("myNav").style.width = "0%";
-// }
